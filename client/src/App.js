fdbb9ad7136d6e9ca7d9cd6dac4949334e1a8bef
Tijani Fulani <Fulani07@gmail.com>
2021-07-25 11:42:13 +0100

    ..

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Todos from './components/Todos';
 import PreLoader from './components/Preloader';
 import Header from './components/Header';
+import TodoInput from './components/TodoInput';
 
 function App() {
   const [todos, setTodos] = useState(null);
@@ -16,10 +17,15 @@ function App() {
     getTodos();
   }, []);
 
+  const createTodo = async (text) => {
+    const res = await axios.post('http://localhost:5001', { message: text });
+    setTodos(res.data);
+  };
   return (
     <div className='App'>
       <div className='container'>
         <Header />
+        <TodoInput createTodo={createTodo} />
         {todos ? <Todos todos={todos} /> : <PreLoader />}
       </div>
     </div>
