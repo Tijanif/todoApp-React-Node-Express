d572abf8b3c3acea593e571913d97f68c39cbbb7
Tijani Fulani <Fulani07@gmail.com>
2021-07-25 10:26:29 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+
+app.use(express.json());
+
+let todos = [
+  {
+    message: 'Wash car',
+    id: 1,
+  },
+  {
+    message: 'Go for a run',
+    id: 2,
+  },
+  {
+    message: 'Cook dinner',
+    id: 3,
+  },
+];
+
+app.get('/', (req, res) => {
+  res.status(200).json(todos);
+});
+
+app.post('/', (req, res) => {
+  const newTodo = {
+    message: req.body.message,
+    id: uuidv4(),
+  };
+  todos = [...todos, newTodo];
+  res.status(201).json(todos);
+});
+
+const PORT = 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
