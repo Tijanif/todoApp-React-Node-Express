515000eef181f3b592d907ceae97f80572bcdda1
Tijani Fulani <Fulani07@gmail.com>
2021-07-25 12:31:52 +0100

    todo item css.

--- client/src/components/todoItem.jsx
+++ client/src/components/todoItem.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
+import './TodoItem.css';
 const TodoItem = ({ todo }) => {
   return (
     <div className='todoItem'>
       <h1>{todo.message}</h1>
-      <p>{todo.id}</p>
     </div>
   );
 };
